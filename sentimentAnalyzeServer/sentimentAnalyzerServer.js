2a7c6a0fe26e2c131c633f67121ab92ae4bcf896
MargaritaPopova <margarita.kistanova@gmail.com>
2021-03-27 11:16:30 +0000

    port

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -101,7 +101,7 @@ app.get("/text/sentiment", (req,res) => {
     });
 });
 
-let server = app.listen(5560, () => {
+let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
